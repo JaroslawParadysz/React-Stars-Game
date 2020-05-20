4f2b7043e3b249d5f0a555631c6947c5a4f6413b
jaroslaw.paradysz <jaroslaw.paradysz@gmail.com>
2020-05-20 10:53:00 +0200

    Refactoring

--- src/Utils.js
+++ src/Utils.js
@@ -19,31 +19,36 @@ function generateRandomRange(min, max) {
     return range;
 }
 
+function getStarsCount(toSkip) {
+    var newIds = createRange(1, 9);
+    newIds = newIds.filter(x => toSkip.find(y => y === x) === undefined);
+    var propositions = [...newIds];
+
+    if (propositions.length === 0) {
+      return 0;
+    }
+
+    for (let i of newIds) {          
+      for (let j of newIds) {
+        if (i === j) {
+            continue;
+        }
+        
+        if (i + j <= 9) {
+          propositions.push(i + j);
+        }
+      }
+    }
+
+    propositions = [...new Set(propositions)]; 
+    const index = Math.floor((propositions.length-1) * Math.random());
+    return propositions[index];
+}
+
 const utils = {
     createRange: createRange,
     generateRandomRange: generateRandomRange,
-    getStarsCount: function(toSkip) {
-        let newIds = createRange(1, 9);
-        newIds = newIds.filter(x => toSkip.find(y => y === x) === undefined);
-        let propositions = [...newIds];
-    
-        if (propositions.length === 0) {
-          return 0;
-        }
-    
-        for (let i of newIds) {
-          let c = newIds.filter(x => x !== i);
-          for (let j of c) {
-            if (i + j <= 9) {
-              propositions.push(i + j);
-            }
-          }
-        }
-    
-        propositions = [...new Set(propositions)]; 
-        const index = Math.floor((propositions.length-1) * Math.random());
-        return propositions[index];
-      }
+    getStarsCount: getStarsCount,
 }
 
 export default utils;
